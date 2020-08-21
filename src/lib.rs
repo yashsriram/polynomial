b7777319a047157f477dbde11ed9fe0503b5a4a1
Yashasvi Sriram <yash.3997@gmail.com>
2020-08-20 22:37:25 -0500

    simplifies polynomial!, impls AddAssign, Mul traits, derives PartialEq trait, moves tests from main.rs to test fns 	- in polynomial! macro, replaces polynomial::Polynomial::new() with Polynomial::new(); this way it can be used in lib.rs too

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 use std::collections::HashMap;
 use std::fmt;
-use std::ops::{Add, Sub};
+use std::ops::{Add, AddAssign, Mul, Sub};
 
 #[macro_export]
 macro_rules! polynomial (
-    ($($key:expr => $value:expr),*) => (
+    ($($power:expr => $coeff:expr),*) => (
         {
-            let mut p = polynomial::Polynomial::new();
+            let mut p = Polynomial::new();
             $(
-                p.insert($key, $value);
+                p.insert($power, $coeff);
             )*
             p
         }
     );
 );
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Polynomial {
     coeff_of_power: HashMap<i32, f32>,
 }
@@ -61,6 +61,20 @@ impl Add for Polynomial {
     }
 }
 
+impl AddAssign for Polynomial {
+    fn add_assign(&mut self, other: Self) {
+        for (&power, &coeff) in other.coeff_of_power.iter() {
+            self.insert(
+                power,
+                match self.coeff_of_power.get(&power) {
+                    Some(&prev_coeff) => prev_coeff + coeff,
+                    None => coeff,
+                },
+            );
+        }
+    }
+}
+
 impl Sub for Polynomial {
     type Output = Self;
 
@@ -76,5 +90,58 @@ impl Sub for Polynomial {
             );
         }
         difference
+    }
+}
+
+impl Mul for Polynomial {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        let mut product = Polynomial::new();
+        for (&a_power, &a_coeff) in self.coeff_of_power.iter() {
+            let mut term_mul = Polynomial::new();
+            for (&b_power, &b_coeff) in other.coeff_of_power.iter() {
+                // FIXME
+                term_mul.insert(a_power + b_power, a_coeff * b_coeff);
+            }
+            product += term_mul;
+        }
+        product
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{polynomial, Polynomial};
+
+    #[test]
+    fn add() {
+        let p = polynomial! { 1 => 11.0, 2 => 57.0, 0 => 51.0 };
+        let q = polynomial! { 3 => 73.0, 2 => -118.0, 0 => 40.0 };
+        assert_eq!(
+            p + q,
+            polynomial! { 3 => 73.0, 2 => -61.0, 1 => 11.0, 0 => 91.0 }
+        );
+    }
+
+    #[test]
+    fn add_assign() {
+        let mut p = polynomial! { 1 => 11.0, 2 => 57.0, 0 => 51.0 };
+        let q = polynomial! { 3 => 73.0, 2 => -118.0, 0 => 40.0 };
+        p += q;
+        assert_eq!(
+            p,
+            polynomial! { 3 => 73.0, 2 => -61.0, 1 => 11.0, 0 => 91.0 }
+        );
+    }
+
+    #[test]
+    fn sub() {
+        let p = polynomial! { 1 => 11.0, 2 => 57.0, 0 => 51.0 };
+        let q = polynomial! { 3 => 73.0, 2 => -118.0, 0 => 40.0 };
+        assert_eq!(
+            p - q,
+            polynomial! { 3 => -73.0, 2 => 175.0, 1 => 11.0, 0 => 11.0 }
+        );
     }
 }
